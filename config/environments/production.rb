a3b4ca3c7bd35c693ca0634febbc32f0be3ff90f
La-cosa <ibrahimmousa1996@hotmail.com>
2018-03-27 09:56:38 +0200

    Updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cbm64_interface_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = '<cbm64-interface.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
